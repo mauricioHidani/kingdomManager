970679c0630d3f58c751b0e10414e2cda9c757ba
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2024-01-04 11:31:23 -0300

    feat: impl use case find by id

--- backend/infrastructure/src/main/java/com/kingdom/manager/infrastructure/address/persists/repositories/AddressGatewayImpl.java
+++ backend/infrastructure/src/main/java/com/kingdom/manager/infrastructure/address/persists/repositories/AddressGatewayImpl.java
@@ -4,6 +4,9 @@ import com.kingdom.manager.application.address.gateway.AddressGateway;
 import com.kingdom.manager.domain.address.models.Address;
 import com.kingdom.manager.infrastructure.address.mappers.AddressMapper;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class AddressGatewayImpl implements AddressGateway {
 
     private final JpaAddressRepository repository;
@@ -16,6 +19,11 @@ public class AddressGatewayImpl implements AddressGateway {
         var result = repository.save(
                 AddressMapper.toEntity(address)
         );
+        return AddressMapper.toModel(result);
+    }
+
+    @Override public Optional<Address> findById(UUID id) {
+        var result = repository.findById(id);
         return AddressMapper.toModel(result);
     }
 
